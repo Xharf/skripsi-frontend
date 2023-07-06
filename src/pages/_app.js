ade7d8b541675508468d6335b29e4e8fb76b7b13
xharf <123190123@student.upnyk.ac.id>
2023-07-06 19:18:07 +0700

    add meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,18 @@ import "@/styles/globals.css"
 import { Container } from "@mui/material"
 import { useState } from "react"
 import { IdentifyContext } from "../../context/IdentifyContext"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }) {
 	const [identify, setIdentify] = useState(null)
 
 	return (
-		<Container maxWidth="xs">
-			<IdentifyContext.Provider value={{ identify, setIdentify }}>
-				<Component {...pageProps} />
-			</IdentifyContext.Provider>
-		</Container>
+		<>
+			<Container maxWidth="xs">
+				<IdentifyContext.Provider value={{ identify, setIdentify }}>
+					<Component {...pageProps} />
+				</IdentifyContext.Provider>
+			</Container>
+		</>
 	)
 }
